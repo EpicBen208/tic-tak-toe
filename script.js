beb52d358d28360b86dd99ca8191d976d93a08e6
Ben <127615469+EpicBen208@users.noreply.github.com>
2025-09-07 18:22:47 -0400

    tiny fix

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ const gameboard=(function(){
            board[0][0]=='[X]'&&board[1][1]=='[X]'&&board[2][2]=='[X]'||
            board[0][2]=='[X]'&&board[1][1]=='[X]'&&board[2][0]=='[X]'
         ){
-            winner.innerHTML=('Winner: X Wins')
+            winner.innerHTML=('Winner: X')
         }
         else if(board[0][0]=='[O]'&&board[0][1]=='[O]'&&board[0][2]=='[O]'||
             board[1][0]=='[O]'&&board[1][1]=='[O]'&&board[1][2]=='[O]'||
@@ -54,7 +54,7 @@ const gameboard=(function(){
            board[0][0]=='[O]'&&board[1][1]=='[O]'&&board[2][2]=='[O]'||
            board[0][2]=='[O]'&&board[1][1]=='[O]'&&board[2][0]=='[O]'
         ){
-            winner.innerHTML=('Winner: O Wins')
+            winner.innerHTML=('Winner: O')
         }
     }
 return{X,O,display,checkWin}
